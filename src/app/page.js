1b52c23faee450d99a078ae6aef0d45476e33d58
Diego González Cruz <diego@thinkey.cl>
2023-12-13 15:49:12 -0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,26 @@
+// import VideoSlider from "@/components/VideoSlider";
+import dynamic from "next/dynamic";
+const VideoSlider = dynamic(() => import("@/components/VideoSlider"), {
+  ssr: false,
+});
+
+export default function Home() {
+  const videos = [
+    "/assets/loops/hls/slide1.m3u8",
+    "/assets/loops/hls/slide2.m3u8",
+    "/assets/loops/hls/slide3.m3u8",
+    "/assets/loops/hls/slide4.m3u8",
+    "/assets/loops/hls/slide5.m3u8",
+    "/assets/loops/hls/slide6.m3u8",
+    "/assets/loops/hls/slide7.m3u8",
+    "/assets/loops/hls/slide8.m3u8",
+    // "/assets/loops/hls/slide9.m3u8",
+    // "/assets/loops/hls/slide10.m3u8",
+    // "/assets/loops/hls/slide11.m3u8",
+    // "/assets/loops/hls/slide12.m3u8",
+    // "/assets/loops/hls/slide13.m3u8",
+    // "/assets/loops/hls/slide14.m3u8",
+  ];
+
+  return <VideoSlider videos={videos} />;
+}
---
+++ src/components/VideoSlider.jsx
@@ -0,0 +1,70 @@
+"use client";
+import { useEffect } from "react";
+import Reveal from "reveal.js";
+import "reveal.js/dist/reveal.css";
+import ReactPlayer from "react-player";
+
+const VideoSlider = ({ videos }) => {
+  useEffect(() => {
+    const deck = new Reveal({
+      // Display presentation control arrows
+      controls: true,
+
+      // Help the presentation scale to fit the browser window
+      width: "100%",
+      height: "100%",
+      margin: 0,
+      minScale: 1,
+      maxScale: 1,
+
+      // Hides address bar on mobile devices
+      hideAddressBar: true,
+
+      // Transition style
+      transition: "slide", // none/fade/slide/convex/concave/zoom
+
+      // More options available, but these are good for full size backgrounds
+    });
+
+    deck.initialize();
+
+    deck.on("slidechanged", (event) => {
+      console.log("Slide changed to: ", event.indexh);
+    });
+
+    deck.on("make-it-pop", () => {
+      console.log("✨");
+    });
+
+    return () => {
+      deck.off("slidechanged");
+      deck.off("make-it-pop");
+    };
+  }, []);
+
+  return (
+    <div className="reveal h-screen w-screen debug1">
+      <div className="slides w-full h-full">
+        {videos.map((video, index) => (
+          <section key={index} className="player-wrapper">
+            <ReactPlayer
+              url={video}
+              controls={false}
+              autoPlay={true}
+              playing={true}
+              controls={false}
+              volume={0}
+              muted={true}
+              loop={true}
+              width="100%"
+              height="100%"
+              className="react-player"
+            />
+          </section>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default VideoSlider;
