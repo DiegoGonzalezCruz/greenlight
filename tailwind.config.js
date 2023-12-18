a50d85690f049f0b07e7dd537d64650d6121f1bd
Diego González Cruz <diego@thinkey.cl>
2023-12-18 09:52:23 -0500

    added plugin for dynamic height

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,24 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".min-h-dscreen": generate("minHeight"),
+        ".h-dscreen": generate("height"),
+      });
+
+      function generate(property) {
+        return {
+          [property]: [
+            "calc(100vh - env(safe-area-inset-bottom, 0) - env(safe-area-inset-top, 0))",
+            "100dvh",
+          ],
+          "@supports (-webkit-touch-callout: none)": {
+            [property]: ["-webkit-fill-available", "100dvh"],
+          },
+        };
+      }
+    }),
+  ],
 };
