32d2f8df0d5ab1a319a5870bf81a634925e1b3c2
Diego González Cruz <diego@thinkey.cl>
2023-12-19 03:35:17 -0500

    fixed

--- src/components/VideoSliderVideoTag.jsx
+++ src/components/VideoSliderVideoTag.jsx
@@ -13,37 +13,33 @@ import {
 } from "swiper/modules";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-
-// Import Swiper styles
 import "swiper/css/bundle";
 
 const VideoSliderVideoTag = ({ videos }) => {
   const router = useRouter();
-  const videoRefs = useRef(videos.map(() => useRef()));
+  const videoRefs = useRef([]);
 
+  // Initial setup for videoRefs
+  useEffect(() => {
+    videoRefs.current = videoRefs.current.slice(0, videos.length);
+  }, [videos]);
 
-
-  // State to keep track of the current slide index
   const [activeSlideIndex, setActiveSlideIndex] = useState(0);
-  // Update the current slide index when it changes
-  // console.log("activeSlideIndex", activeSlideIndex);
+
   const handleSlideChange = (swiper) => {
     setActiveSlideIndex(swiper.activeIndex);
 
-    // Pause all videos
     videoRefs.current.forEach((videoRef, index) => {
       if (videoRef.current && videoRef.current.pause) {
         videoRef.current.pause();
       }
     });
 
-    // Play the video in the current slide
     const currentVideo = videoRefs.current[swiper.activeIndex];
     if (currentVideo && currentVideo.current && currentVideo.current.play) {
       currentVideo.current.play();
     }
   };
-
 
   const handleOnClick = (video) => {
     console.log("click", video);
@@ -64,29 +60,23 @@ const VideoSliderVideoTag = ({ videos }) => {
         ]}
         spaceBetween={0}
         slidesPerView={1}
-        onSlideChange={handleSlideChange} // Add this line
+        onSlideChange={handleSlideChange}
         className="h-full w-full  relative z-50 "
         navigation
         parallax
         mousewheel
         effect="fade"
       >
-        {videos.map((video, index) => {
-          console.log(video)
-          return (
-            <div className="h-full w-full cursor-pointer" key={video.href}>
-              <SwiperSlide
-                key={video.href}
-                className="relative aspect-video cursor-pointer w-full h-full bg-[#203133] z-40"
-              >
-                <video width='100%' height='100%' loop ref={videoRefs.current[index]}>
-                  <source src={video?.hd} />
-                </video>
-              </SwiperSlide>
-            </div>
-          )
-        })}
-
+        {videos.map((video, index) => (
+          <SwiperSlide
+            key={video.href}
+            className="relative aspect-video cursor-pointer w-full h-full bg-[#203133] z-40"
+          >
+            <video width='100%' height='100%' loop ref={el => videoRefs.current[index] = el}>
+              <source src={video?.hd} />
+            </video>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
